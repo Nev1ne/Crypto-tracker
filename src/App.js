20f4dd1a3bdfff46026b997acd17427aead409be
Nevine Achola <nevine.achola@student.moringaschool.com>
2023-07-09 21:25:16 +0300

    update files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
 
+
+https://api.coingecko.com/api/v3/coins/markets?vs_currency=kshs&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en
+
 function App() {
+
+
+
+
   return (
     <div className="App">
       <h1>APIII</h1>
