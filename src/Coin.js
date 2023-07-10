5a30f96f5ee2b9f686750dce8dba428b1e0a0e01
Nevine Achola <nevine.achola@student.moringaschool.com>
2023-07-10 21:27:33 +0300

    update files

--- src/Coin.js
+++ src/Coin.js
@@ -3,7 +3,18 @@ import React from 'react'
 const Coin = () => {
   return (
     <div className='coin-container'>
-        
+        <div className='coin-row'>
+            <div className='coin'>
+                <img src={image} alt='crypto'/>
+                <h1>{name}</h1>
+                <P className='coin-symbol'>{}symbol</P>
+            </div>
+             <div className='coin-data'>
+              <p className='coin-price'>${price}</p>
+              <P className='coin-volume'>${volume.}</P>
+
+             </div>
+        </div>
     </div>
   )
 }
