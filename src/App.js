45c51420c9aac836edae5cbe61740e1b716fbd99
Nevine Achola <nevine.achola@student.moringaschool.com>
2023-07-18 20:52:48 +0300

    final commit

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   const [search, setSearch] = useState('')
 
   useEffect(() => {
-    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=kshs&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en')
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en')
     .then(res => {
       setCoins(res.data);
       console.log(res.data);
